9ebe06746c39820a38b73f63fad7d5d9b6fa850e
Doe Mohamud <doe.mahamud1@gmail.com>
2021-07-28 15:35:18 -0400

    commit tree 70993361a76a7ebf22258664dcbe1e68cafe0872

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  let load = document.getElementById("text")
+  load.textContent = "This is really cool!";
+  }); 
